878582463f01a510dea14271478e271caa564cd0
Aditya Prakarsa <aditya.prakarsa@hyphengroup.io>
2023-11-24 11:51:36 +0800

    feat: adding warehouse page functionalities

--- src/components/Switcher.tsx
+++ src/components/Switcher.tsx
@@ -1,32 +1,48 @@
-import { useState } from 'react'
-import Switch from '@mui/material/Switch'
-import { green } from '@mui/material/colors'
-import CheckIcon from '@mui/icons-material/Check'
+import React from "react";
+import Switch from "@mui/joy/Switch";
+import Typography from "@mui/joy/Typography";
 
 const Switcher = () => {
-  const [isPhysicalLocation, setIsPhysicalLocation] = useState(false)
-  const [isConfirmed, setIsConfirmed] = useState(false)
+    return (
+        <div>
+            <Switch
+                slotProps={{
+                    track: {
+                        children: (
+                            <React.Fragment>
+                                <Typography
+                                    component="span"
+                                    level="inherit"
+                                    sx={{ ml: "10px" }}
+                                >
+                                    是
+                                </Typography>
+                                <Typography
+                                    component="span"
+                                    level="inherit"
+                                    sx={{ mr: "10px" }}
+                                >
+                                    否
+                                </Typography>
+                            </React.Fragment>
+                        ),
+                    },
+                }}
+                sx={{
+                    // "--Switch-thumbRadius": "24px",
+                    // "--Switch-thumbWidth": "65px",
+                    // "--Switch-thumbOffset": "9px",
+                    // "--Switch-thumbSize": "44px",
+                    // "--Switch-trackHeight": "52px",
+                    // "--Switch-trackWidth": "120px",
+                    // "--Switch-trackRadius": "22px",
+                    "--Switch-thumbSize": "30px",
+                    "--Switch-trackWidth": "67px",
+                    "--Switch-trackHeight": "34px",
+                }}
+            />
+        </div>
+    );
+};
 
-  const handleLocationChange = () => {
-    setIsPhysicalLocation((prevValue) => !prevValue)
-  }
-
-  const handleConfirmation = () => {
-    setIsConfirmed(true) // Logic for confirmation
-  }
-  //need adjust the layout
-  return (
-    <div className="self-stretch flex flex-col items-start justify-start gap-[8px] text-center">
-      <div className="rounded-61xl bg-grey-line flex flex-row items-start justify-start p-1 gap-[8px] text-mini text-grey-dark">
-        <Switch
-          checked={isPhysicalLocation}
-          onChange={handleLocationChange}
-          color="primary"
-          inputProps={{ 'aria-label': 'physical location switch' }}
-        />
-      </div>
-    </div>
-  )
-}
-
-export default Switcher
+export default Switcher;
--- src/pages/Collector/Warehouse/Warehouse.tsx
+++ src/pages/Collector/Warehouse/Warehouse.tsx
@@ -1,220 +1,232 @@
-import { FunctionComponent, useCallback, ReactNode, useState } from 'react'
-import { useTheme } from '@mui/material/styles'
-import useMediaQuery from '@mui/material/useMediaQuery'
+import { FunctionComponent, useCallback, ReactNode, useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 // import { useNavigate } from 'react-router-dom'
-import { Box } from '@mui/material'
-import { ADD_ICON } from '../../../themes/icons'
-
-import AddWarehouse from '../../../components/AddWarehouse'
-import TableBase from '../../../components/TableBase'
+import { Box } from "@mui/material";
+import { ADD_ICON } from "../../../themes/icons";
+import AddWarehouse from "../../../components/AddWarehouse";
+import TableBase from "../../../components/TableBase";
+import { useTranslation } from "react-i18next";
 
 interface WarehouseFormData {
-  id: string
-  traditionalName: string
-  simplifiedName: string
-  englishName: string
-  location: string
-  place: string
-  status: string
-  recyclableSubcategories: string
+    id: string;
+    traditionalName: string;
+    simplifiedName: string;
+    englishName: string;
+    location: string;
+    place: string;
+    status: string;
+    recyclableSubcategories: string;
 }
 
 type TableRow = {
-  [key: string]: string | number | null
-}
+    [key: string]: string | number | null;
+};
 
 const Warehouse: FunctionComponent = () => {
-  // const navigate = useNavigate()
-  const [drawerOpen, setDrawerOpen] = useState(false)
-  const theme = useTheme()
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
-  const [action, setAction] = useState<'add' | 'edit' | 'delete'>('add')
-  const [rowId, setRowId] = useState<string>('1')
-  const [warehouseItems, setWarehouseItems] = useState([
-    {
-      id: '1',
-      traditionalName: '火炭',
-      simplifiedName: '火炭',
-      englishName: 'Fo Tan 1',
-      location: '是',
-      place: '火炭拗背灣街14號',
-      status: 'activated',
-      recyclableSubcategories: '紙張、金屬、塑膠、玻璃樽'
-    },
-    {
-      id: '2',
-      traditionalName: '火炭',
-      simplifiedName: '火炭',
-      englishName: 'Fo Tan 2',
-      location: '是',
-      place: '火炭拗背灣街14號',
-      status: 'activated',
-      recyclableSubcategories: '紙張、金屬、塑膠、玻璃樽'
-    },
-    {
-      id: '3',
-      traditionalName: '火炭',
-      simplifiedName: '火炭',
-      englishName: 'Fo Tan 3',
-      location: '是',
-      place: '火炭拗背灣街14號',
-      status: 'activated',
-      recyclableSubcategories: '紙張、金屬、塑膠、玻璃樽'
-    },
-    {
-      id: '4',
-      traditionalName: '火炭',
-      simplifiedName: '火炭',
-      englishName: 'Fo Tan 4',
-      location: '是',
-      place: '火炭拗背灣街14號',
-      status: 'deleted',
-      recyclableSubcategories: '紙張、金屬、塑膠、玻璃樽'
-    }
-  ])
-  const headerTitles = [
-    {
-      type: 'string',
-      field: 'traditionalName',
-      label: '繁體中文名稱'
-    },
-    {
-      type: 'string',
-      field: 'simplifiedName',
-      label: '简体中文名称'
-    },
-    {
-      type: 'string',
-      field: 'englishName',
-      label: 'English Name',
-      width: 150
-    },
-    {
-      type: 'string',
-      field: 'location',
-      label: '實體地點'
-    },
-    {
-      type: 'string',
-      field: 'place',
-      label: '地點'
-    },
-    {
-      type: 'status',
-      field: 'status',
-      label: '狀態',
-      width: 100
-    },
-    {
-      type: 'string',
-      field: 'recyclableSubcategories',
-      label: '回收物類別'
-    }
-  ]
+    // const navigate = useNavigate()
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const { t } = useTranslation();
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+    const [action, setAction] = useState<"add" | "edit" | "delete">("add");
+    const [rowId, setRowId] = useState<string>("1");
+    const [warehouseItems, setWarehouseItems] = useState([
+        {
+            id: "1",
+            traditionalName: "火炭",
+            simplifiedName: "火炭",
+            englishName: "Fo Tan 1",
+            location: "是",
+            place: "火炭拗背灣街14號",
+            status: "activated",
+            recyclableSubcategories: "紙張、金屬、塑膠、玻璃樽",
+        },
+        {
+            id: "2",
+            traditionalName: "火炭",
+            simplifiedName: "火炭",
+            englishName: "Fo Tan 2",
+            location: "是",
+            place: "火炭拗背灣街14號",
+            status: "activated",
+            recyclableSubcategories: "紙張、金屬、塑膠、玻璃樽",
+        },
+        {
+            id: "3",
+            traditionalName: "火炭",
+            simplifiedName: "火炭",
+            englishName: "Fo Tan 3",
+            location: "是",
+            place: "火炭拗背灣街14號",
+            status: "activated",
+            recyclableSubcategories: "紙張、金屬、塑膠、玻璃樽",
+        },
+        {
+            id: "4",
+            traditionalName: "火炭",
+            simplifiedName: "火炭",
+            englishName: "Fo Tan 4",
+            location: "是",
+            place: "火炭拗背灣街14號",
+            status: "deleted",
+            recyclableSubcategories: "紙張、金屬、塑膠、玻璃樽",
+        },
+    ]);
+    const headerTitles = [
+        {
+            type: "string",
+            field: "traditionalName",
+            label: t("warehouse_page.trad_name"),
+            width: 150,
+        },
+        {
+            type: "string",
+            field: "simplifiedName",
+            label: t("warehouse_page.simp_name"),
+            width: 150,
+        },
+        {
+            type: "string",
+            field: "englishName",
+            label: t("warehouse_page.english_name"),
+            width: 150,
+        },
+        {
+            type: "string",
+            field: "location",
+            label: t("warehouse_page.location"),
+            width: 100,
+        },
+        {
+            type: "string",
+            field: "place",
+            label: t("warehouse_page.place"),
+            width: 100,
+        },
+        {
+            type: "status",
+            field: "status",
+            label: t("warehouse_page.status"),
+            width: 100,
+        },
+        {
+            type: "string",
+            field: "recyclableSubcategories",
+            label: t("warehouse_page.recyclable_subcategories"),
+            width: 200,
+        },
+    ];
 
-  const addDataWarehouse = () => {
-    setDrawerOpen(true)
-    setAction('add')
-  }
+    const addDataWarehouse = () => {
+        setDrawerOpen(true);
+        setAction("add");
+    };
 
-  const handleEdit = (type: string, row: TableRow) => {
-    setRowId(row.id as string)
-    setDrawerOpen(true)
-    setAction('edit')
-  }
+    const handleEdit = (type: string, row: TableRow) => {
+        setRowId(row.id as string);
+        setDrawerOpen(true);
+        setAction("edit");
+    };
 
-  const handleDelete = (type: string, row: TableRow) => {
-    setDrawerOpen(true)
-    setAction('delete')
-    setRowId(row.id as string)
-  }
+    const handleDelete = (type: string, row: TableRow) => {
+        setDrawerOpen(true);
+        setAction("delete");
+        setRowId(row.id as string);
+    };
 
-  const handleDrawerClose = () => {
-    setDrawerOpen(false)
-  }
+    const handleDrawerClose = () => {
+        setDrawerOpen(false);
+    };
 
-  const handleOnSubmitData = (
-    formData: WarehouseFormData,
-    action: string,
-    id?: string
-  ) => {
-    if (action == 'add') {
-      //real case use post api
-      setWarehouseItems([...warehouseItems, formData])
-    }
+    const handleOnSubmitData = (
+        formData: WarehouseFormData,
+        action: string,
+        id?: string
+    ) => {
+        if (action == "add") {
+            //real case use post api
+            setWarehouseItems([...warehouseItems, formData]);
+        }
 
-    if (action == 'delete') {
-      //real case use delete api base on id
-      // const { idRow } = id
-      // if (idRow) {
-      const updatedItems = warehouseItems.filter((item) => item.id !== id)
-      setWarehouseItems(updatedItems)
-      // }
-    }
+        if (action == "delete") {
+            //real case use delete api base on id
+            // const { idRow } = id
+            // if (idRow) {
+            const updatedItems = warehouseItems.filter(
+                (item) => item.id !== id
+            );
+            setWarehouseItems(updatedItems);
+            // }
+        }
 
-    if (action == 'edit') {
-      //real case use put api
-      //setWarehouseItems([...warehouseItems, formData])
-    }
-  }
+        if (action == "edit") {
+            //real case use put api
+            //setWarehouseItems([...warehouseItems, formData])
+        }
+    };
 
-  return (
-    <Box
-      sx={{
-        width: '100%',
-        maxWidth: { xs: 375, sm: 480, md: '100%' }
-      }}
-    >
-      <div className="warehouse-section">
-        <div className="settings-page relative bg-bg-primary w-full h-[1046px] overflow-hidden flex flex-row items-start justify-start text-center text-mini text-grey-darker font-tag-chi-medium">
-          <div className=" self-stretch flex-1 bg-white flex flex-col items-start justify-start text-smi text-grey-middle font-noto-sans-cjk-tc">
-            <div className="self-stretch flex-1 bg-bg-primary flex flex-col items-start justify-start text-3xl text-black font-tag-chi-medium">
-              <div
-                className={`settings-container self-stretch flex-1 flex flex-col items-start justify-start pt-[30px] pb-[75px] text-mini text-grey-darker ${
-                  isMobile
-                    ? 'overflow-auto whitespace-nowrap w-[375px] mx-4 my-0'
-                    : 'px-10'
-                }`}
-              >
-                <div className="self-stretch flex flex-col items-start justify-start gap-[12px] overflow-auto">
-                  <div className="settings-header self-stretch flex flex-row items-center justify-start gap-[12px] text-base text-grey-dark">
-                    <b className="relative tracking-[0.08em] leading-[28px]">
-                      工場
-                    </b>
-                    <div
-                      className="rounded-6xl bg-white overflow-hidden flex flex-row items-center justify-center py-2 pr-5 pl-3 gap-[5px] cursor-pointer text-smi text-green-primary border-[1px] border-solid border-green-pale"
-                      onClick={addDataWarehouse}
-                    >
-                      <ADD_ICON />
-                      <b className="relative tracking-[1px] leading-[20px]">
-                        新增
-                      </b>
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                maxWidth: { xs: 375, sm: 480, md: "100%" },
+            }}
+        >
+            <div className="warehouse-section">
+                <div className="settings-page relative bg-bg-primary w-full h-[1046px] overflow-hidden flex flex-row items-start justify-start text-center text-mini text-grey-darker font-tag-chi-medium">
+                    <div className=" self-stretch flex-1 bg-white flex flex-col items-start justify-start text-smi text-grey-middle font-noto-sans-cjk-tc">
+                        <div className="self-stretch flex-1 bg-bg-primary flex flex-col items-start justify-start text-3xl text-black font-tag-chi-medium">
+                            <div
+                                className={`settings-container self-stretch flex-1 flex flex-col items-start justify-start pt-[30px] pb-[75px] text-mini text-grey-darker ${
+                                    isMobile
+                                        ? "overflow-auto whitespace-nowrap w-[375px] mx-4 my-0"
+                                        : "px-10"
+                                }`}
+                            >
+                                <div className="self-stretch flex flex-col items-start justify-start gap-[12px] overflow-auto">
+                                    <div className="settings-header self-stretch flex flex-row items-center justify-start gap-[12px] text-base text-grey-dark">
+                                        <b className="relative tracking-[0.08em] leading-[28px]">
+                                            {t("top_menu.workshop")}
+                                        </b>
+                                        <div
+                                            className="rounded-6xl bg-white overflow-hidden flex flex-row items-center justify-center py-2 pr-5 pl-3 gap-[5px] cursor-pointer text-smi text-green-primary border-[1px] border-solid border-green-pale"
+                                            onClick={addDataWarehouse}
+                                        >
+                                            <ADD_ICON />
+                                            <b className="relative tracking-[1px] leading-[20px]">
+                                                {t("top_menu.add_new")}
+                                            </b>
+                                        </div>
+                                    </div>
+                                    <Box className="w-full">
+                                        <TableBase
+                                            header={headerTitles}
+                                            dataRow={warehouseItems}
+                                            onDelete={(type, row) =>
+                                                handleDelete(action, row)
+                                            }
+                                            onEdit={(type, row) =>
+                                                handleEdit(action, row)
+                                            }
+                                        />
+                                    </Box>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                  <Box className="w-full">
-                    <TableBase
-                      header={headerTitles}
-                      dataRow={warehouseItems}
-                      onDelete={(type, row) => handleDelete(action, row)}
-                      onEdit={(type, row) => handleEdit(action, row)}
-                    />
-                  </Box>
+                    {/* right drawer */}
+                    <AddWarehouse
+                        drawerOpen={drawerOpen}
+                        handleDrawerClose={handleDrawerClose}
+                        action={action}
+                        onSubmitData={handleOnSubmitData}
+                        rowId={rowId}
+                    ></AddWarehouse>
                 </div>
-              </div>
             </div>
-          </div>
-          {/* right drawer */}
-          <AddWarehouse
-            drawerOpen={drawerOpen}
-            handleDrawerClose={handleDrawerClose}
-            action={action}
-            onSubmitData={handleOnSubmitData}
-            rowId={rowId}
-          ></AddWarehouse>
-        </div>
-      </div>
-    </Box>
-  )
-}
+        </Box>
+    );
+};
 
-export default Warehouse
+export default Warehouse;
