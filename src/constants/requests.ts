51ca7700c12a6adde21d8bf5163e0dd89b8d60ec
FHKAD\hong_wong <hong_wong@hk.fujitsu.com>
2023-11-27 11:02:54 +0800

    update api request url

--- src/constants/requests.ts
+++ src/constants/requests.ts
@@ -3,97 +3,92 @@ import { AxiosRequestConfig } from 'axios';
 //tenant manage
 export const LOGIN: AxiosRequestConfig = {
   method: 'post',
-  url: 'api/ac/login',
+  url: 'api/v1/account/login',
 };
 
 export const ADD_TENANT: AxiosRequestConfig = {
   method: 'patch',
-  url: 'api/ac/tenant/astd/addTenant',
+  url: 'api/v1/account/tenant/astd/addTenant',
 };
 
 export const GET_ALL_TENANT: AxiosRequestConfig = {
   method: 'get',
-  url: 'api/ac/tenant/astd/tenant'
+  url: 'api/v1/account/tenant/astd/tenant'
 }
 
 export const GET_TENANT_BY_TENANT_ID: AxiosRequestConfig = {
   method: 'get',
-  url: 'api/ac/tenantInvite'
+  url: 'api/v1/account/tenantInvite'
 }
 
 export const UPDATE_TENANT_REGISTER: AxiosRequestConfig = {
   method: 'patch',
-  url: 'api/ac/tenantInvite'
+  url: 'api/v1/account/tenantInvite'
 }
 
 //collection point
 export const GET_ALL_COLLECTIONPOINT: AxiosRequestConfig = {
   method: 'get',
-  url: 'api/collectors/collectionPoint'
+  url: 'api/v1/collectors/collectionPoint'
 }
 
 export const CREATE_COLLECTIONPOINT: AxiosRequestConfig = {
   method: 'post',
-  url: 'api/collectors/collectionPoint'
+  url: 'api/v1/collectors/collectionPoint'
 }
 
 export const UPDATE_COLLECTIONPOINT: AxiosRequestConfig = {
   method: 'put',
-  url: 'api/collectors/collectionPoint'
-}
-
-export const FIND_COLLECTIONPOINT_EXIST_BYNAME: AxiosRequestConfig = {
-  method: 'get',
-  url: 'api/collectors/collectionPoint/byName'
+  url: 'api/v1/collectors/collectionPoint'
 }
 
 export const FIND_COLLECTIONPOINT_EXIST_BYADDRESS: AxiosRequestConfig = {
   method: 'get',
-  url: 'api/collectors/collectionPoint/byAddress'
+  url: 'api/v1/collectors/collectionPoint/byAddress'
 }
 
 export const GET_CONTRACT: AxiosRequestConfig = {
   method: 'get',
-  url: 'api/collectors/contract'
+  url: 'api/v1/collectors/contract'
 }
 
 //common
 export const GET_COLLECTIONPOINT_TYPE: AxiosRequestConfig = {
   method: 'get',
-  url: 'api/common/colPointType'
+  url: 'api/v1/administrator/colPointType'
 }
 
 export const ADD_COLLECTIONPOINT_TYPE: AxiosRequestConfig = {
   method: 'post',
-  url: 'api/common/colPointType'
+  url: 'api/v1/administrator/colPointType'
 }
 
 export const GET_PREMISE_TYPE: AxiosRequestConfig = {
   method: 'get',
-  url: 'api/common/premiseType'
+  url: 'api/v1/administrator/premiseType'
 }
 
 export const ADD_PREMISE_TYPE: AxiosRequestConfig = {
   method: 'post',
-  url: 'api/common/premiseType'
+  url: 'api/v1/administrator/premiseType'
 }
 
 export const GET_SITE_TYPE: AxiosRequestConfig = {
   method: 'get',
-  url: 'api/common/siteType'
+  url: 'api/v1/administrator/siteType'
 }
 
 export const ADD_SITE_TYPE: AxiosRequestConfig = {
   method: 'post',
-  url: 'api/common/siteType'
+  url: 'api/v1/administrator/siteType'
 }
 
 export const GET_RECYC_TYPE: AxiosRequestConfig = {
   method: 'get',
-  url: 'api/common/recycType'
+  url: 'api/v1/administrator/recycType'
 }
 
 export const ADD_RECYC_TYPE: AxiosRequestConfig = {
   method: 'post',
-  url: 'api/common/recycType'
+  url: 'api/v1/administrator/recycType'
 }
