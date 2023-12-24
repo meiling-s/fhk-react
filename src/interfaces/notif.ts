b2c5f988e32ea45ac6f0e01c51923fa15aa3c754
asp12fhksimon <asp12fhksimon@gmail.com>
2023-12-24 19:52:45 +0800

    add notif data structure

--- src/interfaces/notif.ts
+++ src/interfaces/notif.ts
@@ -1,3 +1,19 @@
 export type Notif = {
   title: string
 }
+// {
+//   "notiRecordId": 1,
+//   "loginId": "string",
+//   "messageType": "SMS",
+//   "title": "string",
+//   "content": "string",
+//   "sender": "string",
+//   "receiver": "string",
+//   "exeDatetime": "2023-12-22T09:49:24.801",
+//   "status": "SUCCESS",
+//   "createdBy": "string",
+//   "updatedBy": "string",
+//   "readFlg": false,
+//   "createdAt": "2023-12-23T12:47:51.249364",
+//   "updatedAt": "2023-12-23T12:47:51.259048"
+// },
