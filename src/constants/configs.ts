66d8d7f541a3ba59284b4e728aa53b902d0466c8
windaharyani <150983206+windaharyani@users.noreply.github.com>
2024-01-11 15:44:04 +0700

    change general url for api request

--- src/constants/configs.ts
+++ src/constants/configs.ts
@@ -1,8 +1,11 @@
 export const AXIOS_DEFAULT_CONFIGS = Object.freeze({
   baseURL: {
-    account: 'http://10.166.22.107:8001/',
-    administrator: 'http://10.166.22.107:8002/',
-    collector: 'http://10.166.22.107:8003/',
+    // account: 'http://10.166.22.107:8001/',
+    // administrator: 'http://10.166.22.107:8002/',
+    // collector: 'http://10.166.22.107:8003/',
+    account: 'http://10.166.22.250/',
+    administrator: 'http://10.166.22.250/',
+    collector: 'http://10.166.22.250/',
   },
   timeout: 100000,
   headers: {
