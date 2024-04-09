663e187c9d4df374ed0502598149e2ad5ac620b1
Laurentius <laurentiuskevin44@gmail.com>
2024-04-09 09:53:22 +0700

    init SBI-00380

---
+++ src/APICalls/userManufacturer.ts
@@ -0,0 +1,72 @@
+import { AXIOS_DEFAULT_CONFIGS } from "../constants/configs";
+import {
+  GET_USER_MANUFACTURER_LIST,
+  CREATE_USER_MANUFACTURER,
+  UPDATE_USER_MANUFACTURER,
+  DELETE_USER_MANUFACTURER,
+} from "../constants/requests";
+import { returnApiToken } from "../utils/utils";
+import axiosInstance from "../constants/axiosInstance";
+
+// get all the user account
+export const getAllUserManufacturer = async (page: number, size: number) => {
+  const token = returnApiToken();
+  try {
+    const response = await axiosInstance({
+      baseURL: AXIOS_DEFAULT_CONFIGS.baseURL.collector,
+      ...GET_USER_MANUFACTURER_LIST(token.tenantId),
+      params: {
+        page: page,
+        size: size,
+      },
+    });
+    return response;
+  } catch (e) {
+    console.error("Get all User Account failed:", e);
+    return null;
+  }
+};
+
+export const postUserManufacturer = async (data: any) => {
+  try {
+    const response = await axiosInstance({
+      baseURL: AXIOS_DEFAULT_CONFIGS.baseURL.collector,
+      ...CREATE_USER_MANUFACTURER,
+      data: data,
+    });
+    return response;
+  } catch (e) {
+    console.error(" create User Account failed:", e);
+    return null;
+  }
+};
+
+export const updateUserManufacturer = async (loginId: string, data: any) => {
+  const token = returnApiToken();
+  try {
+    const response = await axiosInstance({
+      baseURL: AXIOS_DEFAULT_CONFIGS.baseURL.collector,
+      ...UPDATE_USER_MANUFACTURER(token.tenantId, loginId),
+      data: data,
+    });
+    return response;
+  } catch (e) {
+    console.error("update User Account failed:", e);
+    return null;
+  }
+};
+
+export const deleteUserAccount = async (loginId: string, data: any) => {
+  const token = returnApiToken();
+  try {
+    const response = await axiosInstance({
+      baseURL: AXIOS_DEFAULT_CONFIGS.baseURL.collector,
+      ...DELETE_USER_MANUFACTURER(loginId),
+      data: data,
+    });
+    return response;
+  } catch (e) {
+    console.error("delete User Account failed:", e);
+    return null;
+  }
+};
