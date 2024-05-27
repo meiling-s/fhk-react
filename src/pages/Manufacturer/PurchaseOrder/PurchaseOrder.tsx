66559f9b0e0cf53625c1211fffde70ac5275d4c2
Nelson Munthe <nelson.m@wistkey.dev>
2024-05-27 13:14:11 +0700

    adjust maintenance page

--- src/pages/Manufacturer/PurchaseOrder/PurchaseOrder.tsx
+++ src/pages/Manufacturer/PurchaseOrder/PurchaseOrder.tsx
@@ -502,8 +502,6 @@ const PurchaseOrder = () => {
       const { state, realm } = extractError(error)
       if(state.code === STATUS_CODE[503]){
         navigate('/maintenance')
-      } else {
-        navigate(`/${realm}/error`, {state: state})
       }
     }
   }
