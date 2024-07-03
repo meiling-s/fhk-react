4c963edfd73618674d80c43906d3546cac75950a
windaharyani <150983206+windaharyani@users.noreply.github.com>
2024-07-03 16:38:31 +0700

    fix pickup order list

--- src/components/PickupOrderList.tsx
+++ src/components/PickupOrderList.tsx
@@ -20,17 +20,8 @@ import CustomAutoComplete from './FormComponents/CustomAutoComplete'
 import { useContainer } from 'unstated-next'
 import { getAllPickUpOrder } from '../APICalls/Collector/pickupOrder/pickupOrder'
 import { queryPickupOrder } from '../interfaces/pickupOrder'
-import { Languages, localStorgeKeyName } from '../constants/constant'
+import { localStorgeKeyName } from '../constants/constant'
 import { getAllLogisticsPickUpOrder } from '../APICalls/Collector/pickupOrder/pickupOrder'
-import i18n from '../setups/i18n'
-
-interface StatusPickUpOrder {
-  value: string
-  labelEng: string,
-  labelSchi: string,
-  labelTchi: string
-}
-
 interface AddWarehouseProps {
   drawerOpen: boolean
   handleDrawerClose: () => void
@@ -50,7 +41,6 @@ const PickupOrderList: FunctionComponent<AddWarehouseProps> = ({
   const { t } = useTranslation()
   const [picoList, setPicoList] = useState<PicoRefrenceList[]>([])
   const [filteredPico, setFilteredPico] = useState<PicoRefrenceList[]>([])
-  const [filtered, setFiltered] = useState<PicoRefrenceList[]>([])
   const [pickupOrder, setPickupOrder] = useState<PickupOrder[]>()
   const [query, setQuery] = useState<queryPickupOrder>({
     picoId: '',
@@ -63,7 +53,6 @@ const PickupOrderList: FunctionComponent<AddWarehouseProps> = ({
   })
   const role = localStorage.getItem(localStorgeKeyName.role)
   const [selectedPico, setSelectedPico] = useState<string>('')
-  const [search, setSearch] = useState<string| undefined>(undefined);
 
   const initPickupOrderRequest = async () => {
     let result = null
@@ -115,10 +104,10 @@ const PickupOrderList: FunctionComponent<AddWarehouseProps> = ({
 
       setFilteredPico(filteredData)
       if (filteredData) {
-        setFiltered(filteredData)
+        setFilteredPico(filteredData)
       }
     } else {
-      setFiltered(picoList)
+      setFilteredPico(picoList)
     }
   }
 
@@ -131,89 +120,18 @@ const PickupOrderList: FunctionComponent<AddWarehouseProps> = ({
     }
   }
 
-  const getStatus = (status:string) :string => {
-    let name = ''
-  
-    const statusList: StatusPickUpOrder[] = [
-      {
-        value: '0',
-        labelEng: 'CREATED',
-        labelSchi: '待处理',
-        labelTchi: '待處理'
-      },
-      {
-        value: '1',
-        labelEng: 'STARTED',
-        labelSchi: '处理中',
-        labelTchi: '處理中'
-      },
-      {
-        value: '2',
-        labelEng: 'CONFIRMED',
-        labelSchi: '已确认',
-        labelTchi: '已確認'
-      },
-      {
-        value: '3',
-        labelEng: 'REJECTED',
-        labelSchi: '已拒绝',
-        labelTchi: '已拒絕'
-      },
-      {
-        value: '4',
-        labelEng: 'COMPLETED',
-        labelSchi: '已完成',
-        labelTchi: '已完成'
-      },
-      {
-        value: '5',
-        labelEng: 'CLOSED',
-        labelSchi: '已取消',
-        labelTchi: '已取消'
-      },
-      {
-        value: '6',
-        labelEng: 'OUTSTANDING',
-        labelSchi: '已逾期',
-        labelTchi: '已逾期'
-      },
-      {
-        value: '',
-        labelEng: t('localizedTexts.filterValueAny'),
-        labelSchi: '任何',
-        labelTchi: '任何'
-      },
-    ]
-
-    const data = statusList.find(item => item.labelEng === status)
-    if(data && i18n.language === Languages.ENUS){
-      name = data.labelEng
-    } else if(data && i18n.language === Languages.ZHCH){
-      name = data.labelSchi
-    } else if(data){
-      name = data.labelTchi
-    }
-    
-    return name
-  }
-
-  const onCloseDrawer = () => {
-    handleDrawerClose()
-    setSearch(undefined)
-  }
-
   return (
     <>
       <div>
         <RightOverlayForm
           open={drawerOpen}
-          onClose={onCloseDrawer}
+          onClose={handleDrawerClose}
           anchor={'right'}
           action="none"
           headerProps={{
             title: t('pick_up_order.select_po'),
             subTitle: '',
-            onCloseHeader: onCloseDrawer
+            onCloseHeader: handleDrawerClose
           }}
         >
           <Box>
@@ -269,7 +187,7 @@ const PickupOrderList: FunctionComponent<AddWarehouseProps> = ({
                   </CustomField>
                 </div>
                 <Box>
-                  {!search && filteredPico.map((item, index) =>
+                  {filteredPico.map((item, index) =>
                     item.status != 'CLOSED' ? (
                       <div
                         key={index}
@@ -289,58 +207,7 @@ const PickupOrderList: FunctionComponent<AddWarehouseProps> = ({
                         </div>
                         <div className="date-type mb-2 flex items-center gap-2">
                           <div className="text-smi bg-green-200 text-green-600 px-2 py-3 rounded-[50%]">
-                            {getStatus(item.status)}
-                          </div>
-                          <div className="text-smi text-[#717171]">
-                            {item.effFrmDate}
-                          </div>
-                          <div className="text-smi text-[#717171]">
-                            {t('pick_up_order.to')}
-                          </div>
-                          <div className="text-smi text-[#717171]">
-                            {item.effToDate}
-                          </div>
-                          <div className="text-smi text-[#717171]">
-                            {item.routine}
-                          </div>
-                        </div>
-                        <div className="mb- flex items-center gap-2">
-                          <div>
-                            <img src="../Delivery.svg" alt="" />
-                          </div>
-                          <div className="text-xs text-[#717171]">
-                            {item.senderName}
-                          </div>
-                          <div className="text-xs text-[#717171]">
-                            {item.receiver}
-                          </div>
-                        </div>
-                      </div>
-                    ) : (
-                      <></>
-                    )
-                  )}
-                  {search && filtered.map((item, index) =>
-                    item.status != 'CLOSED' ? (
-                      <div
-                        key={index}
-                        onClick={() => {
-                          handleSelectedPicoId(
-                            item.pickupOrderDetail,
-                            item.picoId
-                          )
-                        }}
-                        className="card-pico p-4 border border-solid rounded-lg border-grey-line cursor-pointer mb-4"
-                      >
-                        <div className="font-bold text-mini mb-2">
-                          {item.type}
-                        </div>
-                        <div className="text-smi mb-2 text-[#717171]">
-                          {item.picoId}
-                        </div>
-                        <div className="date-type mb-2 flex items-center gap-2">
-                          <div className="text-smi bg-green-200 text-green-600 px-2 py-3 rounded-[50%]">
-                            {getStatus(item.status)}
+                            {item.status}
                           </div>
                           <div className="text-smi text-[#717171]">
                             {item.effFrmDate}
