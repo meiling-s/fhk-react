8068ae0d6d7687707ee7ce560e6b586bfb998ff2
David <chandradavid19@gmail.com>
2024-07-30 15:51:44 +0700

    fix notif for web

--- src/contexts/NotifContainer.ts
+++ src/contexts/NotifContainer.ts
@@ -34,29 +34,28 @@ const Notification = () => {
 
   //  } 
   // }, [loginId])
-
-  useEffect(() => {
-    if(loginId){
-      setNotifList([])
-      setNumOfNotif(0)
-      getNumNotif(loginId)
-      getNotifList(loginId)
-      const interval = setInterval(() => {
-        initBroadcastMessage()
-      }, 60 * 60000);
   
-      return() => {
-        clearInterval(interval)
-      }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      initBroadcastMessage()
+    }, 60 * 60000);
+
+    return() => {
+      clearInterval(interval)
     }
-  }, [loginId])
+  }, [])
+  useEffect(() => {
+    setNotifList([])
+    setNumOfNotif(0)
+    getNumNotif(loginId)
+    getNotifList(loginId)
+  }, [])
 
   const getNumNotif = async (loginId: string) => {
     const result = await getNumUnreadNotif(loginId)
     const data = result?.data
-
     if (result?.status === 200) {
-      setNumOfNotif(prev => prev + Number(data))
+      setNumOfNotif(data)
     }
   }
 
@@ -64,9 +63,7 @@ const Notification = () => {
     const result = await getNotifByUserId(loginId)
     const data = result?.data
     if (data) {
-      setNotifList((prev:Notif[]) => {
-        return [...prev, ...data]
-      })
+      setNotifList(data)
     }
   }
 
