081bd8dd35568a4c36799670b4cbfca38e0bceb0
fadlydev <fadly.s@wistkey.dev>
2024-10-22 12:00:21 +0700

    AP2CD-23 Update theme

--- src/themes/palette.ts
+++ src/themes/palette.ts
@@ -1,43 +1,61 @@
-import { Palette } from '@mui/icons-material';
 import { createTheme } from '@mui/material';
-import { orange } from '@mui/material/colors';
 import { PaletteOptions } from '@mui/material/styles/createPalette';
 
-
 declare module '@mui/material/styles' {
-    interface Theme {
-      status: {
-        danger: string;
-      };
-    }
-    // allow configuration using `createTheme`
-    interface ThemeOptions {
-      status?: {
-        danger?: string;
-      };
-    }
+  interface Theme {
+    status: {
+      danger: string;
+    };
   }
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
 
 export const paletteColors = {
   KeyGreen: '#79CA25',
-  Green1:'#7CE495',
-  Green2:'#E4F6DC',
-  Red:'#FFF0F4',
-  Red1:'#FF4242',
-  Grey:'#ACACAC',
-  Blue:'#6BC7FF'
+  Green1: '#7CE495',
+  Green2: '#E4F6DC',
+  Red: '#FFF0F4',
+  Red1: '#FF4242',
+  Grey: '#ACACAC',
+  Blue: '#6BC7FF'
 };
 
-
 const theme = createTheme({
-    palette: {
-        primary: {
-          main: paletteColors.KeyGreen,
-          light:paletteColors.Green2
+  palette: {
+    primary: {
+      main: paletteColors.KeyGreen,
+      light: paletteColors.Green2
+    },
+    secondary: {
+      main: '#edf2ff',
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        containedPrimary: {
+          backgroundColor: paletteColors.KeyGreen,
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: '#6BAF21', // Darker shade for hover state
+          },
         },
-        secondary: {
-          main: '#edf2ff',
+        outlinedPrimary: {
+          borderColor: paletteColors.KeyGreen, // Green border
+          color: paletteColors.KeyGreen, // Green text
+          backgroundColor: '#fff', // White background
+          '&:hover': {
+            borderColor: '#6BAF21', // Darker green border on hover
+            backgroundColor: paletteColors.Green2, // Light green background on hover
+          },
         },
       },
-    });
+    },
+  },
+});
+
 export default theme;
