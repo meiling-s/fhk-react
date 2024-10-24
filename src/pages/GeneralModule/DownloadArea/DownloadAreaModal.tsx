20d724b662216cfb2a8dbf5f582ebb50886d9078
bimapaundra <paundra.b@wistkey.dev>
2024-10-24 08:09:57 +0700

    epd report formula integration

--- src/pages/GeneralModule/DownloadArea/DownloadAreaModal.tsx
+++ src/pages/GeneralModule/DownloadArea/DownloadAreaModal.tsx
@@ -76,17 +76,6 @@ const DownloadAreaModal: FunctionComponent<DownloadModalProps> = ({
   staffId,
   userRole,
 }) => {
-  const initialFormula: formulaItem[] = [
-    {
-      id: 1,
-      label: "以回收公司出貨作準",
-    },
-    {
-      id: 2,
-      label: "以工廠入貨作準",
-    },
-  ];
-
   const { t } = useTranslation();
   const [startDate, setStartDate] = useState<dayjs.Dayjs>(dayjs());
   const [endDate, setEndDate] = useState<dayjs.Dayjs>(dayjs());
@@ -97,7 +86,19 @@ const DownloadAreaModal: FunctionComponent<DownloadModalProps> = ({
   const [trySubmited, setTrySubmited] = useState<boolean>(false);
   const [validation, setValidation] = useState<formValidate[]>([]);
   const [tenant, setTenant] = useState<string>("");
-  const [selectedFormula, setSelectedFormula] = useState<number>(1);
+  const [selectedFormula, setSelectedFormula] = useState<number>(0);
+
+  const initialFormula: formulaItem[] = [
+    {
+      id: 0,
+      label: t("generate_report.formula_check_in"),
+    },
+    {
+      id: 1,
+      label: t("generate_report.formula_check_out"),
+    },
+  ];
+
   const [formulaList, setFormulaList] = useState<formulaItem[]>(initialFormula);
 
   useEffect(() => {
@@ -106,7 +107,7 @@ const DownloadAreaModal: FunctionComponent<DownloadModalProps> = ({
     } else {
       setTrySubmited(true);
     }
-  }, [startDate, endDate, i18n.language, validation]);
+  }, [startDate, endDate, i18n.language, validation, selectedFormula]);
 
   useEffect(() => {
     getReport();
@@ -376,7 +377,7 @@ const DownloadAreaModal: FunctionComponent<DownloadModalProps> = ({
               startDate
             ).format("YYYY-MM-DD 00:00:00")}&toDate=${dayjs(endDate).format(
               "YYYY-MM-DD 23:59:59"
-            )}`;
+            )}&checkType=${selectedFormula}`;
           break;
         default:
           url =
@@ -408,6 +409,7 @@ const DownloadAreaModal: FunctionComponent<DownloadModalProps> = ({
     setStartDate(dayjs());
     setEndDate(dayjs());
     setTenant("");
+    setSelectedFormula(0);
   };
 
   const onCloseDrawer = () => {
