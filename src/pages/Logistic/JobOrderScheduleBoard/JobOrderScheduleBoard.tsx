0d245740f8f76a6bf3dea831c6616406c261737b
Ryan Syach <ryan.a@wistkey.dev>
2024-11-27 22:23:26 +0700

    feat: Implementing autologin for embedded web

--- src/pages/Logistic/JobOrderScheduleBoard/JobOrderScheduleBoard.tsx
+++ src/pages/Logistic/JobOrderScheduleBoard/JobOrderScheduleBoard.tsx
@@ -1,11 +1,14 @@
-import { Stack } from '@mui/material'
 import React from 'react'
+import { Stack } from '@mui/material'
 import { useTranslation } from 'react-i18next'
+import { returnApiToken } from 'src/utils/utils'
 
 type Props = {}
 
 export default function JobOrderScheduleBoard({ }: Props) {
     const { i18n } = useTranslation()
+    const { tenantId } = returnApiToken()
+    
     let lang = 'en'
     switch(i18n.language) {
         case 'enus':
@@ -20,7 +23,7 @@ export default function JobOrderScheduleBoard({ }: Props) {
     }
 
     const baseURL: string = window.baseURL.socif
-    const iFrameURL: string = `${baseURL}#/manage/assign-order?embedded=true&lang=${lang}`
+    const iFrameURL: string = `${baseURL}#/login?astd-tenantId=${tenantId}&password=astd-${tenantId}pw&redirect=/manage/assign-order&embedded=true&lang=${lang}`
 
     return (
         <Stack sx={{padding: '1rem'}}>
--- src/pages/Logistic/VehicleRouteTracker/VehicleRouteTracker.tsx
+++ src/pages/Logistic/VehicleRouteTracker/VehicleRouteTracker.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Stack } from '@mui/material'
 import { useTranslation } from 'react-i18next'
+import { returnApiToken } from 'src/utils/utils'
 
 type Props = {}
 
 export default function VehicleRouteTracker({ }: Props) {
     const { i18n } = useTranslation()
+    const { tenantId } = returnApiToken()
+
     let lang = 'en'
     switch(i18n.language) {
         case 'enus':
@@ -18,8 +21,9 @@ export default function VehicleRouteTracker({ }: Props) {
             lang = 'zh'
             break
     }
+
     const baseURL: string = window.baseURL.socif
-    const iFrameURL: string = `${baseURL}#/manage/trace-view?embedded=true&lang=${lang}`
+    const iFrameURL: string = `${baseURL}#/login?astd-tenantId=${tenantId}&password=astd-${tenantId}pw&redirect=/manage/trace-view&embedded=true&lang=${lang}`
 
     return (
         <Stack sx={{padding: '1rem'}}>
