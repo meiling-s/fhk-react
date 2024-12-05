498bee629c5fc16e7ad6f9a6b9eb9c707b3406d9
Lendus Chan <lendus@hk.fujitsu.com>
2024-12-05 11:11:19 +0000

    Update CustomCopyrightSection.tsx (1-0-0beta-385)

--- src/components/CustomCopyrightSection.tsx
+++ src/components/CustomCopyrightSection.tsx
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 const CustomCopyrightSection = () => {
   return (
     <Typography sx={{ color: "white", marginBottom: "20px" }}>
-      Copyright by GreenHoop 2024 <br /> v1-0-0beta-384
+      Copyright by GreenHoop 2024 <br /> v1-0-0beta-385
     </Typography>
   )
 }
