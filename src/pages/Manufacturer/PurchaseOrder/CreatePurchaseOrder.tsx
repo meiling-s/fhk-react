3a0386bd857e585d40f87c9e630cd8f087a5a0e3
David <david.c@wistkey.dev>
2025-01-02 12:51:44 +0700

    fix when create purchase order product item is not sended correctly

--- src/pages/Manufacturer/PurchaseOrder/CreatePurchaseOrder.tsx
+++ src/pages/Manufacturer/PurchaseOrder/CreatePurchaseOrder.tsx
@@ -1,138 +1,165 @@
-import { useFormik } from 'formik'
-import PurchaseOrderCreateForm from '../../../components/FormComponents/PurchaseOrderCreateForm'
-import { PurChaseOrder, PurchaseOrderDetail } from '../../../interfaces/purchaseOrder'
-import { useNavigate } from 'react-router'
-import { useState } from 'react'
-import * as Yup from 'yup'
-import { useTranslation } from 'react-i18next'
-import { extractError, returnApiToken, showErrorToast } from '../../../utils/utils'
-import { STATUS_CODE, Status, localStorgeKeyName } from '../../../constants/constant'
-import { postPurchaseOrder } from '../../../APICalls/Manufacturer/purchaseOrder'
-import { refactorPurchaseOrderDetail } from './utils'
-
+import { useFormik } from "formik";
+import PurchaseOrderCreateForm from "../../../components/FormComponents/PurchaseOrderCreateForm";
+import {
+  PurChaseOrder,
+  PurchaseOrderDetail,
+} from "../../../interfaces/purchaseOrder";
+import { useNavigate } from "react-router";
+import { useState } from "react";
+import * as Yup from "yup";
+import { useTranslation } from "react-i18next";
+import {
+  extractError,
+  returnApiToken,
+  showErrorToast,
+} from "../../../utils/utils";
+import {
+  STATUS_CODE,
+  Status,
+  localStorgeKeyName,
+} from "../../../constants/constant";
+import { postPurchaseOrder } from "../../../APICalls/Manufacturer/purchaseOrder";
+import { refactorPurchaseOrderDetail } from "./utils";
 
 const CreatePurchaseOrder = () => {
-  const navigate = useNavigate()
-  const [addRow, setAddRow] = useState<PurchaseOrderDetail[]>([])
-  const { t } = useTranslation()
-  const realm = localStorage.getItem(localStorgeKeyName.realm)
-  const loginId = localStorage.getItem(localStorgeKeyName.username) || ''
+  const navigate = useNavigate();
+  const [addRow, setAddRow] = useState<PurchaseOrderDetail[]>([]);
+  const { t } = useTranslation();
+  const realm = localStorage.getItem(localStorgeKeyName.realm);
+  const loginId = localStorage.getItem(localStorgeKeyName.username) || "";
 
   function getTenantId() {
     const tenantId = returnApiToken().decodeKeycloack.substring(
-      'company'.length
-    )
+      "company".length
+    );
 
-    return tenantId
+    return tenantId;
   }
 
   const getErrorMsg = (field: string, type: string) => {
     switch (type) {
-      case 'empty':
-        return field + ' ' + t('form.error.shouldNotBeEmpty')
-      case 'atLeastOnePicoExist':
-        return field + ' ' + t('form.error.atLeastOnePicoExist')
-      case 'isInWrongFormat':
-        return field + ' ' + t('form.error.isInWrongFormat')
+      case "empty":
+        return field + " " + t("form.error.shouldNotBeEmpty");
+      case "atLeastOnePicoExist":
+        return field + " " + t("form.error.atLeastOnePicoExist");
+      case "isInWrongFormat":
+        return field + " " + t("form.error.isInWrongFormat");
     }
-  }
+  };
 
   const validateSchema = Yup.object().shape({
     receiverName: Yup.string().required(
-      t('purchase_order.create.this') + ' ' +
-      t('purchase_order.create.receiving_company_name') + ' ' +
-      t('purchase_order.create.is_required')
+      t("purchase_order.create.this") +
+        " " +
+        t("purchase_order.create.receiving_company_name") +
+        " " +
+        t("purchase_order.create.is_required")
     ),
     contactName: Yup.string().required(
-      t('purchase_order.create.this') + ' ' +
-      t('purchase_order.create.contact_name') + ' ' +
-      t('purchase_order.create.is_required')
+      t("purchase_order.create.this") +
+        " " +
+        t("purchase_order.create.contact_name") +
+        " " +
+        t("purchase_order.create.is_required")
     ),
     contactNo: Yup.string().required(
-      t('purchase_order.create.this') + ' ' +
-      t('purchase_order.create.contact_number') + ' ' +
-      t('purchase_order.create.is_required')
+      t("purchase_order.create.this") +
+        " " +
+        t("purchase_order.create.contact_number") +
+        " " +
+        t("purchase_order.create.is_required")
     ),
     paymentType: Yup.string().required(
-      t('purchase_order.create.this') + ' ' +
-      t('purchase_order.create.payment_method') + ' ' +
-      t('purchase_order.create.is_required')
+      t("purchase_order.create.this") +
+        " " +
+        t("purchase_order.create.payment_method") +
+        " " +
+        t("purchase_order.create.is_required")
     ),
     purchaseOrderDetail: Yup.array()
-      .required(getErrorMsg(t('pick_up_order.recyle_loc_info'), 'empty'))
+      .required(getErrorMsg(t("pick_up_order.recyle_loc_info"), "empty"))
       .test(
-        'has-rows',
-        getErrorMsg(t('pick_up_order.recyle_loc_info'), 'empty')!!,
+        "has-rows",
+        getErrorMsg(t("pick_up_order.recyle_loc_info"), "empty")!!,
         (value) => {
-          return value.length > 0 || addRow.length > 0
+          return value.length > 0 || addRow.length > 0;
         }
-      )
-  })
+      ),
+  });
 
   const submitPurchaseOrder = async (values: PurChaseOrder) => {
     try {
-      return await postPurchaseOrder(values)
+      return await postPurchaseOrder(values);
     } catch (error: any) {
       const { state, realm } = extractError(error);
       if (state.code === STATUS_CODE[503]) {
-        navigate('/maintenance')
+        navigate("/maintenance");
       } else {
-        return null
+        return null;
       }
     }
-  }
+  };
 
-
-  const currentDate = new Date().toISOString()
+  const currentDate = new Date().toISOString();
   const createPurchaseOrder = useFormik({
     initialValues: {
-      poId: '',
-      picoId: '',
+      poId: "",
+      picoId: "",
       cusTenantId: getTenantId(),
-      sellerTenantId: '',
-      senderAddr: '',
+      sellerTenantId: "",
+      senderAddr: "",
       senderAddrGps: [0],
-      senderName: '',
-      receiverName: '',
-      contactName: '',
-      contactNo: '',
-      paymentType: '',
+      senderName: "",
+      receiverName: "",
+      contactName: "",
+      contactNo: "",
+      paymentType: "",
       status: Status.CREATED,
-      approvedAt: '',
-      rejectedAt: '',
-      approvedBy: '',
-      rejectedBy: '',
+      approvedAt: "",
+      rejectedAt: "",
+      approvedBy: "",
+      rejectedBy: "",
       createdBy: loginId,
       updatedBy: loginId,
       createdAt: currentDate,
       updatedAt: currentDate,
       purchaseOrderDetail: [],
     },
-    // validationSchema: validateSchema,
     onSubmit: async (values: PurChaseOrder) => {
+      const refactorDetail = refactorPurchaseOrderDetail(addRow);
+      refactorDetail.map((value) => {
+        if (value.productType) {
+          value.productTypeId = value.productType;
+        }
+        if (value.productSubType) {
+          value.productSubTypeId = value.productSubType;
+        }
+        if (value.productAddonType) {
+          value.productAddonTypeId = value.productAddonType;
+        }
+      });
+      values.purchaseOrderDetail = refactorDetail;
 
-      const refactorDetail  = refactorPurchaseOrderDetail(addRow)
-      values.purchaseOrderDetail = refactorDetail
-      const result = await submitPurchaseOrder(values)
-      const data = result?.data
+      const result = await submitPurchaseOrder(values);
+      const data = result?.data;
 
       if (data) {
-        navigate(`/${realm}/purchaseOrder`, { state: 'created' })
+        navigate(`/${realm}/purchaseOrder`, { state: "created" });
       } else {
-        showErrorToast('fail to create purchase order')
+        showErrorToast("fail to create purchase order");
       }
-    }
-  })
+    },
+  });
 
   return (
     <PurchaseOrderCreateForm
       formik={createPurchaseOrder}
-      title={t('purchase_order.create.create_order')}
+      title={t("purchase_order.create.create_order")}
       setState={setAddRow}
       state={addRow}
       editMode={false}
     />
-  )
-}
+  );
+};
 
-export default CreatePurchaseOrder
+export default CreatePurchaseOrder;
