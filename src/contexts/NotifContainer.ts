7d62c7af2273b300151654672245b85e72515841
David <david.c@wistkey.dev>
2025-03-03 12:35:42 +0700

    fix broadcast msg behaviour

--- src/contexts/NotifContainer.ts
+++ src/contexts/NotifContainer.ts
@@ -65,48 +65,49 @@ const Notification = () => {
   }
 
   const initBroadcastMessage = async () => {
-    const result = await getBroadcastMessage()
-      if (result?.length >= 1) {
-        let broadcast: Broadcast = {}
-        for(let message of result){
-          const isBefore = dayjs().isBefore(message.effToDate, 'day');
-          const isSame = dayjs().isSame(message.effToDate, 'day');
-          if(isSame || isBefore){
-            broadcast.effFromDate = message.effFromDate;
-            broadcast.effToDate = message.effToDate;
+    const result = await getBroadcastMessage();
+    if (result?.length) {
+        let broadcast: Partial<Broadcast> = {};
+        let availableLanguages: Set<string> = new Set();
 
-            switch (message.lang) {
-              case getSelectedLanguange(Languages.ZHCH):
-                broadcast.title_schi = message.title;
-                broadcast.content_schi = message.content;
-                break;
-              case getSelectedLanguange(Languages.ZHHK):
-                broadcast.title_tchi = message.title;
-                broadcast.content_tchi = message.content;
-                break;
-              default:
-                broadcast.title_enus = message.title;
-                broadcast.content_enus = message.content;
-                break;
+        for (let message of result) {
+            const isValid = dayjs().isSameOrBefore(message.effToDate, 'day');
+            if (isValid) {
+                availableLanguages.add(message.lang);
+                broadcast.effFromDate = message.effFromDate;
+                broadcast.effToDate = message.effToDate;
+
+                switch (message.lang) {
+                    case getSelectedLanguange(Languages.ZHCH):
+                        broadcast.title_schi = message.title;
+                        broadcast.content_schi = message.content;
+                        break;
+                    case getSelectedLanguange(Languages.ZHHK):
+                        broadcast.title_tchi = message.title;
+                        broadcast.content_tchi = message.content;
+                        break;
+                    default:
+                        broadcast.title_enus = message.title;
+                        broadcast.content_enus = message.content;
+                        break;
+                }
             }
-          }
         }
-        
-        setBroadcast(prev => {
-          if(prev?.title_enus == broadcast?.title_enus && prev?.content_enus == broadcast?.content_enus){
-            return prev
-          } else {
-            setShowBroadcast(true)
-            setMarginTop('30px')
-            return broadcast
-          }
-        })
-      }  else {
-        setShowBroadcast(false)
-        setMarginTop('0px')
-        setBroadcast(null)
-      }
-  }
+
+        const selectedLanguage = getSelectedLanguange(i18n.language);
+        const shouldShowBroadcast = availableLanguages.has(selectedLanguage);
+
+        setBroadcast(shouldShowBroadcast ? (broadcast as Broadcast) : null);
+        setShowBroadcast(shouldShowBroadcast);
+        setMarginTop(shouldShowBroadcast ? '30px' : '0px');
+    } else {
+        setBroadcast(null);
+        setShowBroadcast(false);
+        setMarginTop('0px');
+    }
+};
+
+
 
   useEffect(() => {
     initBroadcastMessage()
