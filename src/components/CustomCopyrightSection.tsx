10da3e5884d222af7ae18bdc670ef9dad6dca2b4
Orion Lee <orion_lee@fujitsu.com>
2025-03-19 08:05:09 +0000

    Update CustomCopyrightSection.tsx (1-0-0beta-457)

--- src/components/CustomCopyrightSection.tsx
+++ src/components/CustomCopyrightSection.tsx
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 const CustomCopyrightSection = () => {
   return (
     <Typography sx={{ color: "white", marginBottom: "20px" }}>
-      Copyright by GreenHoop 2025 <br /> v1-0-0beta-456
+      Copyright by GreenHoop 2025 <br /> v1-0-0beta-457
     </Typography>
   )
 }
