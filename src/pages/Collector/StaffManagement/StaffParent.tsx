941228f4ede263a80cddc89788599849994d893e
David <david.c@wistkey.dev>
2025-03-20 14:47:13 +0700

    remove manufacturer tab in logistic module

--- src/pages/Collector/StaffManagement/StaffParent.tsx
+++ src/pages/Collector/StaffManagement/StaffParent.tsx
@@ -1,23 +1,24 @@
-import { FunctionComponent, useCallback, ReactNode, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Tabs from '../../../components/Tabs'
-import { Box, Typography } from '@mui/material'
-import { useTranslation } from 'react-i18next'
-import StaffManagement from './StaffManagement'
-import Rosters from '../Rosters/Rosters'
+import { FunctionComponent, useCallback, ReactNode, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Tabs from "../../../components/Tabs";
+import { Box, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import StaffManagement from "./StaffManagement";
+import Rosters from "../Rosters/Rosters";
 // import UserGroup from '../UserGroup/UserGroup'
-import StaffManufacturer from '../StaffManufacturer/StaffManufacturer'
-import { returnApiToken } from '../../../utils/utils'
-import { localStorgeKeyName } from '../../../constants/constant'
+import StaffManufacturer from "../StaffManufacturer/StaffManufacturer";
+import { returnApiToken } from "../../../utils/utils";
+import { localStorgeKeyName } from "../../../constants/constant";
 
 const Settings: FunctionComponent = () => {
-  const token = returnApiToken()
-  const { t } = useTranslation()
-  const [selectedTab, setSelectedTab] = useState(0)
+  const token = returnApiToken();
+  const { t } = useTranslation();
+  const [selectedTab, setSelectedTab] = useState(0);
   const handleTabChange = (value: number, label: string) => {
-    setSelectedTab(value)
-  }
-  const role = localStorage.getItem(localStorgeKeyName.role) || 'collectoradmin'
+    setSelectedTab(value);
+  };
+  const role =
+    localStorage.getItem(localStorgeKeyName.role) || "collectoradmin";
   // const tabSettings = [
   //   t('staffManagement.list'),
   //   t('staffManagement.schedule'),
@@ -25,28 +26,34 @@ const Settings: FunctionComponent = () => {
   // ]
 
   const tabSettingsCollectors = [
-    t('staffManagement.list'),
-    t('staffManagement.schedule'),
+    t("staffManagement.list"),
+    t("staffManagement.schedule"),
     // t('staffManagement.manufacturer')
-  ]
+  ];
 
-  const tabSettingsNonCollectors = [
-    t('staffManagement.list'),
-    t('staffManagement.manufacturer')
-  ]
+  const tabSettingsManufacturer = [
+    t("staffManagement.list"),
+    t("staffManagement.manufacturer"),
+  ];
+
+  const tabSettingsOthers = [t("staffManagement.list")];
 
   const tabSettings =
-    role === 'collector' ? tabSettingsCollectors : tabSettingsNonCollectors
+    role === "collector"
+      ? tabSettingsCollectors
+      : role === "manufacturer"
+      ? tabSettingsManufacturer
+      : tabSettingsOthers;
 
   return (
     <Box className="container-wrapper w-max">
       <div className="settings-page bg-bg-primary">
         <Box>
           <Typography fontSize={16} color="black" fontWeight="bold">
-            {t('staffManagement.staff')}
+            {t("staffManagement.staff")}
           </Typography>
         </Box>
-        {token.realmApiRoute !== 'customer' ? (
+        {token.realmApiRoute !== "customer" ? (
           <Tabs
             tabs={tabSettings}
             navigate={handleTabChange}
@@ -54,7 +61,7 @@ const Settings: FunctionComponent = () => {
             className="lg:px-10 sm:px-4 bg-bg-primary"
           />
         ) : null}
-        {role === 'collector' ? (
+        {role === "collector" ? (
           selectedTab === 0 ? (
             <StaffManagement />
           ) : selectedTab === 1 ? (
@@ -73,7 +80,7 @@ const Settings: FunctionComponent = () => {
         )}
       </div>
     </Box>
-  )
-}
+  );
+};
 
-export default Settings
+export default Settings;
