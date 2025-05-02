10be814ff5b1b64c2708c5f49dd6da3e6e51edcf
Ka Ching Lee <kaching_lee@fujitsu.com>
2025-05-02 02:35:48 +0000

    Update CustomCopyrightSection.tsx (1-0-0beta-471 for SIT)

--- src/components/CustomCopyrightSection.tsx
+++ src/components/CustomCopyrightSection.tsx
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 const CustomCopyrightSection = () => {
   return (
     <Typography sx={{ color: "white", marginBottom: "20px" }}>
-      Copyright by GreenHoop 2025 <br /> v1-0-0beta-470
+      Copyright by GreenHoop 2025 <br /> v1-0-0beta-471
     </Typography>
   )
 }
